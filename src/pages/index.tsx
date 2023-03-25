07571e51e97d246671f2a36b269d1d3e84854fe5
Keshav Bhadauria <keshav@vt.edu>
2023-03-25 18:50:23 -0400

    merge conflicts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,31 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import Logout from './logout';
+import {createImage, segmentStory} from './api/test.js';
 
 const inter = Inter({ subsets: ['latin'] })
 
+const prompt = 'yellow ferrari'
+
 export default function Home() {
+  const [text, setText] = useState('');
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(text);
+    // Handle form submission here
+    const Book = await segmentStory(text);
+    // console.log(Book.title + "\n");
+    console.log(Book);
+
+  };
 
   const { user, isLoading } = useAuth();
 
@@ -22,8 +40,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-
-        <h1>{user ? `Hello ${user.email}`: "Hello Guest"}</h1>
+        {/* <button onClick={() => createImage(prompt)}>
+          Hello
+        </button> */}
+        <form onSubmit={handleSubmit}>
+          <label>
+            Input Text:
+            <input type="text" value={text} onChange={handleChange} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        <h1>{user ? `Hello ${user.name}`: "Hello Guest"}</h1>
 
         {user ? <Logout/> : <></>}
              
